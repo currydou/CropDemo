07e15b892bc2351b67b2e2e5e0a9da4d2f5edf1d
curry_dou <274086765@qq.com>
2018-08-04 16:47:32 +0800

    init

---
+++ app/src/main/java/com/example/curry/customcamera/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.curry.customcamera;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "CameraSimple";
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private FrameLayout mCameralayout;
+    private Button mTakePictureBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        if (!checkCameraHardware(this)) {
+            Toast.makeText(CameraActivity.this, "相机不支持", Toast.LENGTH_SHORT)
+                    .show();
+        } else {
+            mCamera = getCameraInstance();
+            mPreview = new CameraPreview(CameraActivity.this, mCamera);
+            mCameralayout = (FrameLayout) findViewById(R.id.camera_preview);
+            mCameralayout.addView(mPreview);
+        }
+        mTakePictureBtn = (Button) findViewById(R.id.button_capture);
+        mTakePictureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamera.takePicture(null, null, mPictureCallback);
+            }
+        });
+    }
+
+    // 判断相机是否支持
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // 获取相机
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
+
+    // 拍照回调
+    private Camera.PictureCallback mPictureCallback = new PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File pictureDir = Environment
+                    .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            if (pictureDir == null) {
+                Log.d(TAG,
+                        "Error creating media file, check storage permissions!");
+                return;
+            }
+
+            try {
+                String pictureName = new DateFormat().format("yyyyMMddHHmmss",
+                        new Date()).toString()
+                        + ".png";
+                FileOutputStream fos = new FileOutputStream(pictureDir
+                        + File.separator + pictureName);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+}
---
+++ ucropdemo/src/main/java/blog/kymjs/org/ucropdemo/MainActivity.java
@@ -0,0 +1,178 @@
+package blog.kymjs.org.ucropdemo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.yalantis.ucrop.UCrop;
+import com.yalantis.ucrop.UCropActivity;
+
+import java.io.File;
+
+import static android.R.attr.maxHeight;
+import static android.R.attr.maxWidth;
+
+public class MainActivity extends BaseActivity {
+
+    private int requestMode = 11;
+    private ImageView ivImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ivImage = (ImageView) findViewById(R.id.ivImage);
+    }
+
+    public void onClick(View view) {
+        pickFromGallery();
+    }
+
+    private void pickFromGallery() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE,
+                    "read_permission",
+                    REQUEST_STORAGE_READ_ACCESS_PERMISSION);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT)
+                    .setType("image/*")
+                    .addCategory(Intent.CATEGORY_OPENABLE);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                String[] mimeTypes = {"image/jpeg", "image/png"};
+                intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+            }
+
+            startActivityForResult(Intent.createChooser(intent, "select_picture"), requestMode);
+        }
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_STORAGE_READ_ACCESS_PERMISSION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickFromGallery();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == requestMode) {
+                final Uri selectedUri = data.getData();
+                if (selectedUri != null) {
+                    Uri uri = Uri.fromFile(new File(getCacheDir(), "111"));
+                    UCrop uCrop = UCrop.of(selectedUri, uri)
+                            .withMaxResultSize(maxWidth, maxHeight);
+                    uCrop = basisConfig(uCrop);
+                    uCrop = advancedConfig(uCrop);
+
+                    uCrop.start(this);
+                } else {
+                }
+            } else if (requestCode == UCrop.REQUEST_CROP) {
+                final Uri resultUri = UCrop.getOutput(data);
+                if (resultUri != null) {
+                    ivImage.setImageURI(resultUri);
+                }
+            }
+        }
+    }
+
+    private UCrop basisConfig(@NonNull UCrop uCrop) {
+//        uCrop = uCrop.useSourceImageAspectRatio();
+                        uCrop = uCrop.withAspectRatio(1, 1);
+//                    uCrop = uCrop.withMaxResultSize(maxWidth, maxHeight);
+
+        return uCrop;
+    }
+
+    /**
+     * Sometimes you want to adjust more options, it's done via {@link com.yalantis.ucrop.UCrop.Options} class.
+     *
+     * @param uCrop - ucrop builder instance
+     * @return - ucrop builder instance
+     */
+    private UCrop advancedConfig(@NonNull UCrop uCrop) {
+        UCrop.Options options = new UCrop.Options();
+
+        options.setCompressionFormat(Bitmap.CompressFormat.PNG);
+        options.setCompressionFormat(Bitmap.CompressFormat.JPEG);
+        options.setHideBottomControls(true);
+        options.setShowCropGrid(false);
+        options.setAllowedGestures(UCropActivity.ALL, UCropActivity.ALL, UCropActivity.ALL);
+//        options.setCompressionQuality(mSeekBarQuality.getProgress());
+
+//        options.setHideBottomControls(mCheckBoxHideBottomControls.isChecked());
+//        options.setFreeStyleCropEnabled(mCheckBoxFreeStyleCrop.isChecked());
+
+        /*
+        If you want to configure how gestures work for all UCropActivity tabs
+
+        options.setAllowedGestures(UCropActivity.SCALE, UCropActivity.ROTATE, UCropActivity.ALL);
+        * */
+
+        /*
+        This sets max size for bitmap that will be decoded from source Uri.
+        More size - more memory allocation, default implementation uses screen diagonal.
+
+        options.setMaxBitmapSize(640);
+        * */
+
+
+       /*
+
+        Tune everything (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
+
+        options.setMaxScaleMultiplier(5);
+        options.setImageToCropBoundsAnimDuration(666);
+        options.setDimmedLayerColor(Color.CYAN);
+        options.setCircleDimmedLayer(true);
+        options.setShowCropFrame(false);
+        options.setCropGridStrokeWidth(20);
+        options.setCropGridColor(Color.GREEN);
+        options.setCropGridColumnCount(2);
+        options.setCropGridRowCount(1);
+        options.setToolbarCropDrawable(R.drawable.your_crop_icon);
+        options.setToolbarCancelDrawable(R.drawable.your_cancel_icon);
+
+        // Color palette
+        options.setToolbarColor(ContextCompat.getColor(this, R.color.your_color_res));
+        options.setStatusBarColor(ContextCompat.getColor(this, R.color.your_color_res));
+        options.setActiveWidgetColor(ContextCompat.getColor(this, R.color.your_color_res));
+        options.setToolbarWidgetColor(ContextCompat.getColor(this, R.color.your_color_res));
+        options.setRootViewBackgroundColor(ContextCompat.getColor(this, R.color.your_color_res));
+
+        // Aspect ratio options
+        options.setAspectRatioOptions(1,
+            new AspectRatio("WOW", 1, 2),
+            new AspectRatio("MUCH", 3, 4),
+            new AspectRatio("RATIO", CropImageView.DEFAULT_ASPECT_RATIO, CropImageView.DEFAULT_ASPECT_RATIO),
+            new AspectRatio("SO", 16, 9),
+            new AspectRatio("ASPECT", 1, 1));
+
+       */
+
+        return uCrop.withOptions(options);
+    }
+
+
+}
